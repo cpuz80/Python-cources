79d5496f7e90fa01978355026f3403dbf2a26eae
cpuz80 <105972445+cpuz80@users.noreply.github.com>
2022-05-21 20:20:34 +0500

    Add files via upload

---
+++ замена_символов_в_строке.py
@@ -0,0 +1,25 @@
+##Заменить в строке "asdxfghyxyx" все буквы "х" на "у"
+a = 'asdxfghyxyx'
+sym = ''
+newsym = ''
+newstring = ''
+##строку в массив
+mass = list(a)
+##обходим массив
+while mass:
+    sym = mass.pop()
+    ##проверка на условие замены
+    if sym == "x":
+      newsym = "y"
+      newstring = newstring + newsym
+    else:
+      newstring = newstring + sym
+reverse = list(newstring)
+##пересобрать строку
+newstring = ''
+while reverse:
+	sym = reverse.pop()
+	newstring = newstring + sym
+print(newstring)
+    
+    
