bebf88f195952dbb1c53349f96c1622da31804e6
cpuz80 <105972445+cpuz80@users.noreply.github.com>
2022-06-18 17:46:29 +0500

    Add files via upload

--- Lection3SearchInList.py
+++ Lection3SearchInList.py
@@ -2,26 +2,33 @@
 animals = ['Dog', 'Cat', 'Bird', 'Fish']
 # #простейшие случаи
 # #искомая константа
-elem = 'Bird'
+ELEM = 'Bird'
 #
 # #перебор элементов в списке
 for animal in animals:
-    #проверка на соответствие
-     if animal == elem:
-         print(animal, ' Найдено!')
+    # проверка на соответствие
+    if animal == ELEM:
+        print(animal, ' Найдено!')
 
 # #через оператор in
-if elem in animals: print(elem, ' Найдено!')
+if ELEM in animals: print(ELEM, ' Найдено!')
 
 # #с помощью лямда-функции
-retrieved_elements = list(filter(lambda x: elem in x, animals))
+retrieved_elements = list(filter(lambda x: ELEM in x, animals))
 print(retrieved_elements, ' Найдено!')
 
 # #используя метод count
-if animals.count(elem) > 0:
-    print(elem, ' Найдено!')
+if animals.count(ELEM) > 0:
+    print(ELEM, ' Найдено!')
 #
-# #через внешний модуль (как надо было в задании))
+# #через мой внешний модуль (как надо было в задании))
 import Lection3Module as mymod
+
 for s in animals:
-    print(s,elem, mymod.funcld(s,elem))
+    print(s, ELEM, mymod.funcld(s, ELEM))
+
+# через внешнюю библиотеку
+import Levenshtein as ExtLibLev
+
+for s in animals:
+    print(s, ELEM, ExtLibLev.setratio(s, ELEM))
